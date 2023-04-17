7b2ed574a74e2482df6fdd2136c64f57e987f0de
Courtney M. Skinner <122307852+SkinnerCourtneyM@users.noreply.github.com>
2023-04-16 21:00:21 -0700

    Add files via upload
    Day 1 of teaching myself how to program!

---
+++ 1. AddingTwoNumbers.py
@@ -0,0 +1,35 @@
+# Python Practice: Adding Two Numbers: 4/16/2023
+
+# Example 1:
+# Input: num1 = 5, num2 = 3
+# Output: 8
+
+# Example 2:
+# Input: num1 = 13, num2 = 6
+# Output: 19
+
+# Python program adding two numbers:
+
+num1 = 15
+num2 = 12
+
+# Adding two nos
+
+sum = num1 + num2
+# Variable one (num1) and Variable two (num2).  Sum = two Varaibles together.
+
+# Print values:
+print(sum)
+
+# Python program adding two numbners provided by user input:
+
+number1 = input("7")
+number2 = input("14")
+
+# Adding two numbers.
+# User might also enter floating points.
+sum = number1 + number2
+print("sum number1 + number2")
+
+# To define a function that takes two intergers and rturn the sum of those two numbers:
+#def = The keyword used to create, (or define) a Function.
