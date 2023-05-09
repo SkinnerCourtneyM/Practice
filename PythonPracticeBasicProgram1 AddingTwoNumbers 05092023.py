1fade69eaa1d39dbdddeef1c2af1deb139541ce7
Courtney M. Skinner <122307852+SkinnerCourtneyM@users.noreply.github.com>
2023-05-09 01:02:46 -0700

    Add files via upload
    Python Practice Basic Program 1 Adding Two Numbers 05/09/2023

---
+++ PythonPracticeBasicProgram1 AddingTwoNumbers 05092023.py
@@ -0,0 +1,13 @@
+#Python Practice: Basic Programs 1 05/09/2023:
+#Adding Two Numbers:
+
+#Delaring the Variables:
+num1 = 15
+num2 = 12
+
+#Adding two numbers:
+#sum = num1 + num2
+
+#Printing values:
+print(num1 + num2)
+#Not a string, no quotations.
